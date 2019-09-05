bcbb9a4e8151001ef956013646e073b8c6b41363
lvzhenchao <1370164516@qq.com>
2019-09-05 14:07:21 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,18 +5,14 @@
 </head>
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">Weibo App</a>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-                <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-            </ul>
-        </div>
-    </nav>
+    {{--头部文件--}}
+    @include('layouts._header')
 
     <div class="container">
         @yield('content')
+
+        {{--尾部子视图--}}
+        @include('layouts._footer')
     </div>
 
 </body>
