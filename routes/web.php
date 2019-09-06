12bbeb9d5691eedd971b39c334e6540127099ab3
lvzhenchao <1370164516@qq.com>
2019-09-06 15:17:44 +0800

    用户页面显示

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 /*注册路由*/
 Route::get('signup', 'UsersController@create')->name('signup');
+//用户资源路由
+Route::resource('users', 'UsersController');
